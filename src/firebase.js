3610916b3631501ab22a0ce1c68db476450cc9b4
D_Zucho <danny.nico.dev@outlook.com>
2022-10-12 02:06:14 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9CJW0idmHnCmK9_6DN63Ncmn-Lp_H8DM',
+  authDomain: 'netflix-clone-yt-c0a81.firebaseapp.com',
+  projectId: 'netflix-clone-yt-c0a81',
+  storageBucket: 'netflix-clone-yt-c0a81.appspot.com',
+  messagingSenderId: '783745515197',
+  appId: '1:783745515197:web:f1dd06ae60e53db14f01d4',
+}
